fad85c224c4b318cbeb87b99f7cfd1e16b13a6ce
erickkf600 <35529628+erickkf600@users.noreply.github.com>
2018-01-26 08:56:21 -0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 //Dados para navegação entre as páginas
 session_start();
 $_SESSION['pagina'] = "index.php";
+$_SESSION['back'] = "login-usuario-2.0.php";
 
 if (isset($_SESSION['usuario'])) {
   $usuario = $_SESSION['usuario'];
@@ -14,45 +15,46 @@ if (isset($_SESSION['usuario'])) {
 <!DOCTYPE html>
 <html> <!--SITE DE LAYOUTS https://www.bootstrapcdn.com/bootswatch/ -->
 <head>
-	<title>ERICK-TECH</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>ERICK-TECH</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-	<link rel="stylesheet" href="css/layout.css">
+    <link rel="stylesheet" href="css/layout.css">
 
-	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="	anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="  anonymous">
 
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Spectral+SC" rel="stylesheet">
 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+    <script src="js/estatisticas-dispositivos-2.0.js"></script>
 
-
+    <script src="js/menu-usuario-2.0.js"></script>
 
 </head>
-
 <body class="body">
 
  <div class="header">
   <a href="index.php">
       <img class="logo" src="css/LOGO.png" alt="logo"></a>
    
+<ul class="menu-top">
+  <li><a class="top" href="login-usuario-2.0.php"><i class="fa fa-user" aria-hidden="true"></i>  Login</a></li>
+  <li><a class="top" href="cadastro-usuarios-2.0.php"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Cadastro</a></li>
+  <li><a class="top" href="contato.php"><i class="fa fa-phone-square" aria-hidden="true"></i> Contato</a></li>
+</ul>
 
+  <ul class="carrinho">
+    <li><a href="#"><b>Carrinho</b>  <i class="fa fa-shopping-cart fa-lg" aria-hidden="true"></i></a></li>
+  </ul>
 
-
-<!--<ul class="carrinho">
-  <li><a href="#"><b>Carrinho</b>  <i class="fa fa-shopping-cart fa-lg" aria-hidden="true"></i></a></li>
- </ul>-->
 
 </div>
 <div id="navbar">
@@ -205,13 +207,8 @@ if (isset($_SESSION['usuario'])) {
 <form>
   <input type="text" id="fname" name="fname" placeholder="Digite seu nome">
   <input class="e-mail" type="text" id="lname" name="lname" placeholder="Seu e-mail">
-  <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#myModal">
-   Enviar
-  </button>
+  <button class="btn btn-danger" type="button" href="#">Enviar</button>
 </form>
-  <?php
-      include "includes/enviar.php"
-    ?>
 </ul>
 </form>
 
