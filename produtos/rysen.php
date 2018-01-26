c116d88c03d0dbd9ca926204447edf5fe86ae554
erickkf600 <35529628+erickkf600@users.noreply.github.com>
2018-01-26 09:05:26 -0200

    Update rysen.php

--- produtos/rysen.php
+++ produtos/rysen.php
@@ -26,12 +26,17 @@
   <a href="../index.php">
       <img class="logo" src="../css/LOGO.png" alt="logo"></a>
    
-  
+  <ul class="menu-top">
+  <li><a class="top" href="login-usuario-2.0.php"><i class="fa fa-user" aria-hidden="true"></i>  Login</a></li>
+  <li><a class="top" href="cadastro-usuarios-2.0.php"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Cadastro</a></li>
+  <li><a class="top" href="contato.php"><i class="fa fa-phone-square" aria-hidden="true"></i> Contato</a></li>
+</ul>
 
-
-  <!--<ul class="carrinho">
+  <ul class="carrinho">
     <li><a href="#"><b>Carrinho</b>  <i class="fa fa-shopping-cart fa-lg" aria-hidden="true"></i></a></li>
-   </ul>-->
+  </ul>
+
+
 </div>
 
 <div id="navbar">
