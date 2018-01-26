14f7eb906b9e5dd4b1bcb93be8e3b97c7240f0f5
erickkf600 <35529628+erickkf600@users.noreply.github.com>
2018-01-26 09:06:28 -0200

    Update video.php

--- produtos/video.php
+++ produtos/video.php
@@ -26,7 +26,7 @@
   <a href="../index.php">
       <img class="logo" src="../css/LOGO.png" alt="logo"></a>
    
-  <!--<ul class="menu-top">
+  <ul class="menu-top">
   <li><a class="top" href="#"><i class="fa fa-user" aria-hidden="true"></i>  Login</a></li>
   <li><a class="top" href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>Cadastro</a></li>
   <li><a class="top" href="#"><i class="fa fa-phone-square" aria-hidden="true"></i>Contato</a></li>
@@ -35,7 +35,7 @@
 
 <ul class="carrinho">
   <li><a href="#"><b>Carrinho</b>  <i class="fa fa-shopping-cart fa-lg" aria-hidden="true"></i></a></li>
- </ul>-->
+ </ul>
 </div>
 
 <div id="navbar">
