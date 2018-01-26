3ee12a190219aae0b7953623e223cbad0dd6f5ec
erickkf600 <35529628+erickkf600@users.noreply.github.com>
2018-01-25 22:36:55 -0200

    Add files via upload

---
+++ includes/enviar.php
@@ -0,0 +1,24 @@
+<div class="modal fade" id="myModal">
+			<div class="modal-dialog">
+				<div class="modal-content">
+      
+						<!-- titulo -->
+		<div class="modal-header">
+			<h4 class="modal-title">Tudo Pronto!</h4>
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+		</div>
+        
+							<!-- Modal body -->
+		<div class="modal-body">
+			<p>Parabéns! Agora você será o primeiro a saber das nossas novidades.<p>
+		</div>
+        
+					<!-- Modal footer -->
+		<div class="modal-footer">
+			<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+		</div>
+
+        
+				</div>
+			</div>
+		</div>
